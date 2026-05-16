ce336e9311a6620cbcf1d6a286832c5e25ad01bf
Denis <andreev.gamepro@gmail.com>
2026-05-16 19:12:20 +0300

    Accept endpoint names in request body

---
+++ internal/server/helpers.go
@@ -0,0 +1,33 @@
+package server
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func Send(w http.ResponseWriter, status int, msg *ResponseContract) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	if err := json.NewEncoder(w).Encode(msg); err != nil {
+		http.Error(w, "failed to encode response", http.StatusInternalServerError)
+	}
+}
+
+func SendSuccess(w http.ResponseWriter, status int, data []byte) {
+	Send(w, status, NewSuccessResponseContract(data))
+}
+
+func SendError(w http.ResponseWriter, status int, msg ErrorContract) {
+	Send(w, status, NewErrorResponseContract(msg))
+}
+
+func DecodeContract[T any](req *http.Request) (*T, error) {
+	var body T
+
+	if err := json.NewDecoder(req.Body).Decode(&body); err != nil {
+		return nil, err
+	}
+
+	return &body, nil
+}
